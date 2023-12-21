5523f2514d318394c7dc09a61f4459fa2f04590f
David Peach <mail@davidpeach.co.uk>
2023-12-21 17:07:16 +0000

    WIP

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello Go!")
+	buffer := []byte("The is the content.\n")
+	os.WriteFile("/tmp/lol", buffer, 0644)
+
+	file, _ := os.OpenFile("/tmp/lol", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+
+	file.WriteString("This comes after.\n")
 }
